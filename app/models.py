094bf49a860291281ae27928e9b60b4d2b7a759e
RanjithPoojary <ranjjithpjr@gmail.com>
2024-11-12 23:38:15 +0530

    Chapter5: Password Hashing

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from typing import Optional
 from datetime import datetime, timezone
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
 
@@ -15,6 +16,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
